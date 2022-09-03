12e7d84ad1584269f9e522a102fa0ec5806d4db5
adminwei <1766990107>
2022-09-03 11:17:11 +0800

    【feat】新增gateway

---
+++ shop-gateway/src/main/java/com/shop/gateway/filter/GatewayAuthFilter.java
@@ -0,0 +1,26 @@
+package com.shop.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Date: 2022/9/3 11:16
+ * @Description: GatewayAuthFilter
+ * @Version 1.0.0
+ */
+@Component
+public class GatewayAuthFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
